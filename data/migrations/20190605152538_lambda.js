822f74b749c6388023063e43d421e1c08cc1ab56
Alyssa Jane <xcthaeh@gmail.com>
2019-06-05 16:30:08 -0400

    created CRUD operations for knex

--- data/migrations/20190605152538_lambda.js
+++ data/migrations/20190605152538_lambda.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("cohorts", table => {
+    table.increments();
+    table.string("name").notNullable();
+    table.string("Track");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("cohorts", table => {});
 };
